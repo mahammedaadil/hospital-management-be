e5b4d13965842c3a1453650ad2e3fc2bfea7f664
mahammedaadil patel <adilchoice30@gmail.com>
2024-11-02 15:41:31 +0530

    Revert "cors changes"
    This reverts commit 04513e3a52fa155e8beefcd071d2844a964f8945.

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ const app= express();
 
 config({path:"./config/config.env"})
 
-app.use(cors());
+app.use(cors({
+    origin:[process.env.FRONTEND_URL,process.env.DASHBOARD_URL],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true,
+
+})
+);
 
 app.use(cookieParser());
 app.use(express.json());
