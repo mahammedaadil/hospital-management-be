048db77e2121e31861eff40cb8708f871af83620
mahammedaadil patel <adilchoice30@gmail.com>
2025-02-17 07:59:53 +0530

    made code understable by others

--- router/appointmentRouter.js
+++ router/appointmentRouter.js
@@ -1,14 +1,19 @@
 import express from "express";
-import { getAllAppointments, postAppointment, updateAppointmentStatus,deleteAppointment,getPatientAppointments,getDoctorAppointments} from "../controller/appointmentController.js";
-import {isDoctorAuthenticated, isAdminAuthenticated ,isPatientAuthenticated,isAuthorized} from "../middlewares/auth.js";
+import { getAllAppointments, postAppointment, updateAppointmentStatus, deleteAppointment, getPatientAppointments, getDoctorAppointments } from "../controller/appointmentController.js";
+import { isDoctorAuthenticated, isAdminAuthenticated, isPatientAuthenticated, isAuthorized } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post("/post",isPatientAuthenticated,postAppointment);
-router.get("/getall",isAuthorized("Admin", "Doctor") ,getAllAppointments);
-router.put("/update/:id",isAuthorized("Admin", "Doctor") ,updateAppointmentStatus);
-router.delete("/delete/:id",isAuthorized("Admin", "Doctor") , deleteAppointment);
-router.get("/getpatient/:id",isPatientAuthenticated, getPatientAppointments);
-router.get("/doctor",isDoctorAuthenticated ,getDoctorAppointments);
+// Patient Routes
+router.post("/post", isPatientAuthenticated, postAppointment); 
+router.get("/getpatient/:id", isPatientAuthenticated, getPatientAppointments); 
+
+// Doctor Routes
+router.get("/doctor", isDoctorAuthenticated, getDoctorAppointments); 
+
+// Admin & Doctor Routes
+router.get("/getall", isAuthorized("Admin", "Doctor"), getAllAppointments); 
+router.put("/update/:id", isAuthorized("Admin", "Doctor"), updateAppointmentStatus); 
+router.delete("/delete/:id", isAuthorized("Admin", "Doctor"), deleteAppointment); 
 
 export default router;
--- router/messageRouter.js
+++ router/messageRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getAllMessages, sendMessage,deleteMessage} from "../controller/messageController.js";
-import {isAuthorized} from "../middlewares/auth.js"; 
+import { getAllMessages, sendMessage, deleteMessage } from "../controller/messageController.js";
+import { isAuthorized } from "../middlewares/auth.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.post("/send",sendMessage);
-router.get("/getall",isAuthorized("Admin", "Doctor") ,getAllMessages);
-router.delete("/delete/:id",isAuthorized("Admin", "Doctor") , deleteMessage);
+// Patient Routes
+router.post("/send", sendMessage); 
+
+// Admin & Doctor Routes
+router.get("/getall", isAuthorized("Admin", "Doctor"), getAllMessages); 
+router.delete("/delete/:id", isAuthorized("Admin", "Doctor"), deleteMessage); 
+
 export default router;
