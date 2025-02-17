35d22799f8e5ee8b942bd59262e6b1d3282e8f97
mahammedaadil patel <adilchoice30@gmail.com>
2025-02-17 08:16:34 +0530

    latest push

--- router/userRouter.js
+++ router/userRouter.js
@@ -10,41 +10,36 @@ import {
   patientRegister,
   deleteDoctor,
   updateDoctor,
-  getDoctorById
+  getDoctorById,
 } from "../controller/userController.js";
-import { isAdminAuthenticated, isPatientAuthenticated, isDoctorAuthenticated, isAuthorized } from "../middlewares/auth.js";
+import {
+  isAdminAuthenticated,
+  isPatientAuthenticated,
+  isDoctorAuthenticated,
+  isAuthorized
+} from "../middlewares/auth.js";
 import { forgotPassword } from "../controller/forgotPasswordController.js";
 import { resetPasswordController } from "../controller/resetPasswordController.js";
 import { verifiedOtpController } from "../controller/verifyOtpController.js";
 
 const router = express.Router();
 
-// Public Routes
-router.post("/patient/register", patientRegister); 
+router.post("/patient/register", patientRegister);
 router.post("/login", login);
+router.post("/admin/addnew", isAdminAuthenticated, addNewAdmin);
+router.get("/doctors", getAllDoctors);
+router.get("/admin/me",isAdminAuthenticated,getUserDetails);
+router.get("/patient/me",isPatientAuthenticated,getUserDetails);
+router.get("/doctor/me",isDoctorAuthenticated,getUserDetails);
+router.get("/admin/logout",isAdminAuthenticated,logoutAdmin);
+router.get("/patient/logout",isPatientAuthenticated, logoutPatient);
+router.post("/doctor/addnew", isAuthorized("Admin", "Doctor") , addNewDoctor);
+router.delete("/doctor/:id", isAuthorized("Admin", "Doctor"), deleteDoctor);
+router.put("/doctor/update/:id", isAuthorized("Admin", "Doctor"), updateDoctor);
+router.get("/doctor/:id", isAdminAuthenticated, getDoctorById);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPasswordController);
+router.post("/verify-otp", verifiedOtpController);
 
-// Admin Routes
-router.post("/admin/addnew", isAdminAuthenticated, addNewAdmin); 
-router.get("/admin/me", isAdminAuthenticated, getUserDetails); 
-router.get("/admin/logout", isAdminAuthenticated, logoutAdmin); 
-router.get("/doctor/:id", isAdminAuthenticated, getDoctorById); 
-
-// Patient Routes
-router.get("/patient/me", isPatientAuthenticated, getUserDetails); 
-router.get("/patient/logout", isPatientAuthenticated, logoutPatient); 
-
-// Doctor Routes
-router.get("/doctor/me", isDoctorAuthenticated, getUserDetails); 
-
-// Admin & Doctor Routes
-router.post("/doctor/addnew", isAuthorized("Admin", "Doctor"), addNewDoctor); 
-router.delete("/doctor/:id", isAuthorized("Admin", "Doctor"), deleteDoctor); 
-router.put("/doctor/update/:id", isAuthorized("Admin", "Doctor"), updateDoctor); 
-router.get("/doctors", getAllDoctors); 
-
-// Password & OTP Routes
-router.post("/forgot-password", forgotPassword); 
-router.post("/reset-password", resetPasswordController); 
-router.post("/verify-otp", verifiedOtpController); 
 
 export default router;
