b48be71be9100a240d738ab782b0489414f8bc0e
mahammedaadil patel <adilchoice30@gmail.com>
2025-02-17 09:17:20 +0530

    fixing profile remain still

--- router/userRouter.js
+++ router/userRouter.js
@@ -11,6 +11,7 @@ import {
   deleteDoctor,
   updateDoctor,
   getDoctorById,
+  getPatientById
 } from "../controller/userController.js";
 import {
   isAdminAuthenticated,
@@ -40,6 +41,6 @@ router.get("/doctor/:id", isAdminAuthenticated, getDoctorById);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPasswordController);
 router.post("/verify-otp", verifiedOtpController);
-
+router.get("/user/info/:id", isAuthorized("Admin", "Doctor", "Patient"), getPatientById);
 
 export default router;
